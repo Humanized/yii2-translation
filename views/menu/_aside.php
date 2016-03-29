7cb35df72b849702d22a080f504fbe6ff0f56ba5
jeffrey <jeffrey@localhost.localdomain>
2016-03-30 01:14:32 +0200

    [MOD] Aside Data included

--- views/menu/_aside.php
+++ views/menu/_aside.php
@@ -1,8 +1,23 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Exemplatory aside menu
+ * 
+ * While this view file is subjective, the usage of GUIHelper is emphasised 
+ * particularly in function of menu setup common to framework practises
+ * 
  */
+use yii\widgets\Menu;
 
+echo Menu::widget([
+    'items' => [
+        ['label' => 'Translations', 'url' => ['/translation/admin/index']],
+        ['label' => 'Languages ', 'url' => ['/translation/languages/index']],
+    ],
+    'options' => [
+        'class' => 'nav nav-pills nav-stacked',
+        'id' => 'navbar-id',
+        'style' => 'font-size: 14px;',
+        'data-tag' => 'yii2-menu',
+    ],
+]);
