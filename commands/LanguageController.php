e3295c1dc94068b9f0bb4cc03a2f77ef8e10420d
jeffrey <jeffrey@localhost.localdomain>
2016-06-01 14:41:11 +0200

    [MOD] CLI for languages functionally complete (first iteration)

--- commands/LanguageController.php
+++ commands/LanguageController.php
@@ -7,7 +7,9 @@ use \humanized\translation\models\Language;
 
 /**
  * 
+ * CLI for dealing with site language administration.
  * 
+ * Allows to set default site language and enable/disable languages 
  * 
  * 
  * @name Translation Module Administration CLI
@@ -21,18 +23,12 @@ use \humanized\translation\models\Language;
 class LanguageController extends Controller
 {
 
-    public function actionEnable($locale)
-    {
-        Language::enable($locale);
-        return 0;
-    }
-
-    public function actionSetDefault($locale)
-    {
-        Language::setDefault($locale);
-        return 0;
-    }
-
+    /**
+     * 
+     * Returns the default site language code, indicating clearly when not set
+     * 
+     * @return int
+     */
     public function actionGetDefault()
     {
         $out = Language::getDefault();
@@ -42,6 +38,49 @@ class LanguageController extends Controller
         }
         $this->stderr('Default language is not set' . "\n");
         return 1;
+    }
+
+    /**
+     * 
+     * @param type $locale
+     * @return int
+     */
+    public function actionSetDefault($locale)
+    {
+        Language::setDefault($locale);
+        return 0;
+    }
+
+    public function actionEnabled()
+    {
+        $this->stdout(implode(', ', Language::enabled())."\n");
+        return 0;
+    }
+
+    /**
+     * 
+     * Enable a language by saving (primary) locale to database storage
+     * 
+     * @param string $locale - Locale to be enabled
+     * @return int - status code
+     */
+    public function actionEnable($locale)
+    {
+        Language::enable($locale);
+        return 0;
+    }
+
+    /**
+     * 
+     * Disable a language by removing locale from database storage
+     * 
+     * @param string $locale - Locale to be enabled
+     * @return int - status code
+     */
+    public function actionDisable($locale)
+    {
+        Language::disable($locale);
+        return 0;
     }
 
 }
