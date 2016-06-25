dc7acfab8a90207f3b820b6bb5cfb55ab31ec98b
jeffrey <jeffrey@localhost.localdomain>
2016-06-25 17:27:52 +0200

    [ADD] Documented for 0.1

--- Module.php
+++ Module.php
@@ -1,21 +1,28 @@
 <?php
 
+/**
+ * @link https://github.com/Humanized/yii2-translation
+ * @copyright Copyright (c) 2016 Humanized BV Comm GCV
+ * @license https://github.com/Humanized/yii2-translation/LICENCE.md
+ */
+
 namespace humanized\translation;
 
 /**
  * 
- * Yii2 Multi-Lingual Site Module.
+ * Yii2 Site Translation Module
  * 
- * Provides interfaces to deal with: 
+ * Provides various interfaces to deal with routine tasks dealing with site translation management.
+ * 
  * - Enable/disable languages using database storage (GUI,CLI)
  * - Set default application language using database storage (GUI,CLI)
  * 
  * Provides various flexible language selection widgets:
  * - Inline list
- * - Dropdown list
- * - Select2 Dropdownlist
+ * - Bootstrap Dropdown button
+ * - Bootstrap Dropdown list
  * 
- * @name Yii2 Translation Module Class 
+ * @name Yii2 Site Translation Module Class 
  * @version 0.1 
  * @author Jeffrey Geyssens <jeffrey@humanized.be>
  * @package yii2-translation
